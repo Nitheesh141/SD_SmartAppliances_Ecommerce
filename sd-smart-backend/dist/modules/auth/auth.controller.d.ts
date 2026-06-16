b8a660335efb166819622c21f24170c5e0a99401
Nitheesh141 <nitheeshvelli141@gmail.com>
2026-06-16 12:30:30 +0530

    Feat:(added login for superadmin and changes for the landing page)

---
+++ sd-smart-backend/dist/modules/auth/auth.controller.d.ts
@@ -0,0 +1,8 @@
+import { Request, Response } from "express";
+export declare const signup: (req: Request, res: Response) => Promise<void>;
+export declare const login: (req: Request, res: Response) => Promise<void>;
+export declare const getMe: (req: Request, res: Response) => Promise<void>;
+export declare const resetPassword: (req: Request, res: Response) => Promise<void>;
+export declare const sendOtp: (req: Request, res: Response) => Promise<void>;
+export declare const loginWithOtp: (req: Request, res: Response) => Promise<void>;
+//# sourceMappingURL=auth.controller.d.ts.map
